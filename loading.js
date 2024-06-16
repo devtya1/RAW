d0377ff68f634ca9e51f818d7709a0acbdb87c6b
devtya1 <98102495+devtya1@users.noreply.github.com>
2024-06-16 18:25:32 +0530

    Add files via upload

--- loading.js
+++ loading.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
+    if(localStorage.getItem("Name") == "Saharsh"){
+        window.location.href = 'training.html';
+    }else{
     // Wait for 5 seconds
     setTimeout(() => {
         // Redirect to the dashboard page
         window.location.href = 'dashboard.html';
     }, 5000);
+}
 });
